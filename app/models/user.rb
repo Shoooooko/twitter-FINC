2f5dd90a3e476f81ba592befc24eb589b6bc28d2
Shoooooko <Amberoshi4sp9@g.ecc.u-tokyo.ac.jp>
2018-12-04 02:22:15 +0900

    user

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :name, presence: true
+  validates :email, uniqueness: true
+  #validates :password, presence: true
   has_many :comments, dependent: :destroy
   has_many :posts, dependent: :destroy
   has_many :fav_posts, dependent: :destroy
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,14 +15,15 @@ RSpec.describe User, type: :model do
   # 名がなければ無効な状態
   it "is invalid without a name" do
     #expect(User.new(name: nil)).not_to be_valid
-    user = User.new(name: nil)
-    user.valid?
-    expect(user.errors[:name]).to include("can't be blank")
+    expect(User.new(name: nil)).not_to be_valid
   end
 
   # パスワードがなければ無効な状態であること
   it "is invalid without a encrypted_password" do
-    expect(User.new(password: nil)).not_to be_valid
+    #expect(User.new(password: nil)).not_to be_valid
+    user = User.new(password: nil)
+    user.valid?
+    expect(user.errors[:password]).to include("can't be blank")
   end
 
   # メールアドレスがなければ無効な状態であること
@@ -31,8 +32,18 @@ RSpec.describe User, type: :model do
   end
 
   # 重複したメールアドレスなら無効な状態であること
-  it "is invalid with a duplicate email address"
-  
+  it "is invalid with a duplicate email address" do
+    User.create(
+      name: "testuser",
+      email: "doublemail@example.com",
+      password: "test_user")
+    user = User.new(
+      name:  "seconduser",
+      email: "doublemail@example.com",
+      password: "second_user")
+    user.valid?
+    expect(user.errors[:email]).to include("has already been taken")
+  end
 
   # ユーザーのフルネームを文字列として返すこと
   it "returns a user's full name as a string"
